efdb073ce354ce77ea7191d367052649c98382be
JanGerber <jan-456@t-online.de>
2016-05-31 09:21:06 +0200

    Update AbstraktesFahrzeug.java

--- src/factoryPattern/demo/AbstraktesFahrzeug.java
+++ src/factoryPattern/demo/AbstraktesFahrzeug.java
@@ -1,6 +1,7 @@
 package factoryPattern.demo;
 
-//Produkt
+//Produkt(Product)
+// von dieser Klasse wird das konkretes Fahrzeug abgeleitet
 public abstract class AbstraktesFahrzeug {
 	private String hersteller;
 	private String modell;
@@ -15,7 +16,8 @@ public abstract class AbstraktesFahrzeug {
 	public int getKw() {
 		return kw;
 	}
-
+		
+	//Konstruktor eines Fahrzeugs
 	public AbstraktesFahrzeug(String hersteller, String modell, int kw) {
 		this.hersteller = hersteller;
 		this.modell = modell;
