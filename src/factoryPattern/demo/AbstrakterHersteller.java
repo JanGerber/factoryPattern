838706b2622081795ff3b6e3febf9891f3d354e4
JanGerber <jan-456@t-online.de>
2016-05-31 09:18:08 +0200

    Update AbstrakterHersteller.java

--- src/factoryPattern/demo/AbstrakterHersteller.java
+++ src/factoryPattern/demo/AbstrakterHersteller.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import factoryPattern.demo.AbstraktesFahrzeug;
 
-//Erzeuger
+// Erzeuger(Creator)
+// von dieser Klasse wird das konkrete Fahrzeug abgeleitet
 public abstract class AbstrakterHersteller {
 	protected List<AbstraktesFahrzeug> fahrzeuge = new ArrayList<AbstraktesFahrzeug>();
 	
-	//Methode delegiert Instanziierung an Unterklasse
+	//Dieser Konstruktor delegiert die Instanzierung des konkreten Objekts an die Unterklasse die diese implementiert
 	public AbstrakterHersteller(){
 		this.erzeugeFahrzeuge();
 	}
@@ -20,7 +21,8 @@ public abstract class AbstrakterHersteller {
 	}
 
 
-	
+	//Diese Methode muss von der Unterklasse(konkretes Fahrzeug) überschrieben und implementiert werden. 
+	//Bei dieser Mehtode handelt es sich um Herzstück der Factory Pattern
 	protected abstract void erzeugeFahrzeuge();
 	
 }
