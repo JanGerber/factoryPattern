12cf24c76618c97cee1b7b9cd8973ba3ee2d8907
JanGerber <jan-456@t-online.de>
2016-05-31 09:23:46 +0200

    Update BMW.java

--- src/factoryPattern/demo/BMW.java
+++ src/factoryPattern/demo/BMW.java
@@ -1,9 +1,10 @@
 package factoryPattern.demo;
 
-//Konkreter Erzeuger
+//Konkreter Erzeuger(ConcreateCreator)
+//instanziiert die konkrete Klasse bzw. in userem Fall das konkrete Produkt
 public class BMW extends AbstrakterHersteller{
 
-	//Implementiert abstrakten Methode
+	//Implementiert abstrakten Methode aus der Oberklasse
 	@Override
 	protected void erzeugeFahrzeuge() {
 		fahrzeuge.add(new I8(231)); // Erzeugung des konkreten Objekts
