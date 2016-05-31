5b5bbcdb84a0e2283e5286945fc5ad4b4cebfbf9
JanGerber <jan-456@t-online.de>
2016-05-31 09:28:35 +0200

    Update I8.java

--- src/factoryPattern/demo/I8.java
+++ src/factoryPattern/demo/I8.java
@@ -1,6 +1,7 @@
 package factoryPattern.demo;
 
-//Konkretes Produkt
+//Konkretes Produkt (Concrete Product)
+//diese Klasse wird von der Factory Methode instanziiert
 public class I8 extends AbstraktesFahrzeug{
 
 	public I8(int kw) {
